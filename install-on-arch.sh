0f1e08a5f3728ec33f695b71ae57c76dce77208c
Stelios Mitas <77465022+MitasTech@users.noreply.github.com>
2022-04-09 18:07:22 +0300

    Update install-on-arch.sh

--- install-on-arch.sh
+++ install-on-arch.sh
@@ -64,20 +64,20 @@ cp -r ./fonts/* ~/.local/share/fonts/
 fc-cache -f
 clear 
 
-echo "We need an AUR helper. It is essential. 1) paru       2) yay"
-read -r -p "What is the AUR helper of your choice? (Default is paru): " num
+#echo "We need an AUR helper. It is essential. 1) paru       2) yay"
+#read -r -p "What is the AUR helper of your choice? (Default is paru): " num
 
-if [ $num -eq 2 ]
-then
-    HELPER="yay"
-fi
+#if [ $num -eq 2 ]
+#then
+#    HELPER="yay"
+#fi
 
-if ! command -v $HELPER &> /dev/null
-then
-    echo "It seems that you don't have $HELPER installed, I'll install that for you before continuing."
-	git clone https://aur.archlinux.org/$HELPER.git ~/.srcs/$HELPER
-	(cd ~/.srcs/$HELPER/ && makepkg -si )
-fi
+#if ! command -v $HELPER &> /dev/null
+#then
+#    echo "It seems that you don't have $HELPER installed, I'll install that for you before continuing."
+#	git clone https://aur.archlinux.org/$HELPER.git ~/.srcs/$HELPER
+#	(cd ~/.srcs/$HELPER/ && makepkg -si )
+#fi
 
 $HELPER -S picom-jonaburg-git\
 	   acpi              \
